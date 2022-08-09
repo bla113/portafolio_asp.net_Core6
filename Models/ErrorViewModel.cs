f0da96fef356d3cc1351d33b7d2cebd6b6c12e0f
bla113 <bla_113@hotmail.com>
2022-08-09 12:38:46 -0600

    iniciando proyecto
    Se inicia Proycto Portafolio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace portafolio_asp.net_Core6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
